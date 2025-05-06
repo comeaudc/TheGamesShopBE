f3f43a29a194ac820c73774c53bd612b2c978927
Dylan Comeau <comeaudc@comcast.net>
2025-05-06 16:15:49 -0400

    Database Setup & Connected

--- server.mjs
+++ server.mjs
@@ -3,15 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import connectDB from './db/conn.mjs';
 import globalErr from "./middleware/globalErr.mjs";
 
 // Setups
+connectDB()
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors());``
 app.use(morgan("tiny"));
 app.use(express.json());
 

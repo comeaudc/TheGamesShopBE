ec22fa277861a80ac42702fd87751e114e417d82
Dylan Comeau <comeaudc@comcast.net>
2025-05-06 17:12:12 -0400

    Added Schemas: User, Cart, Game

--- server.mjs
+++ server.mjs
@@ -3,17 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import connectDB from './db/conn.mjs';
+import connectDB from "./db/conn.mjs";
 import globalErr from "./middleware/globalErr.mjs";
 
 // Setups
-connectDB()
+connectDB();
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());``
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 

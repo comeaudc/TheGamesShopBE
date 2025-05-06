a5b2f28a40b9a5ab2a1221e057b344faecde8ede
Dylan Comeau <comeaudc@comcast.net>
2025-05-06 16:08:20 -0400

    server.mjs boilerplate setup

---
+++ server.mjs
@@ -0,0 +1,26 @@
+// Imports
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import globalErr from "./middleware/globalErr.mjs";
+
+// Setups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+// Routes
+
+// Err Middleware - only run when we have a server error
+app.use(globalErr);
+
+// listener
+app.listen(PORT, () => {
+  console.log(`Server running on Port: ${PORT}`);
+});

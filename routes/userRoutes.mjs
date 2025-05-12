ce284c0d581703e8b76e96f6c5bc404f5327903f
Dylan Comeau <comeaudc@comcast.net>
2025-05-12 12:11:31 -0400

    Created login route and separated user controllers

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,44 +1,16 @@
 import express from "express";
-import User from "../models/userSchema.mjs";
-import Cart from "../models/cartSchema.mjs";
+import userController from "../controllers/userController.mjs";
 
 const router = express.Router();
 
 // @route: POST /api/user/register
 // @desc:  register user route
 // @access: Public
-router.post("/register", async (req, res) => {
-  //Destructure the req.body (opt)
-  const { username, email, password } = req.body;
+router.post("/register", userController.register);
 
-  // Check user submitted all necessary data, if not return
-  if (!username || !email || !password) {
-    return res.status(400).json({ msg: "All fields are required." });
-  }
-
-  // Check if user already exists
-  let user = await User.findOne({ email });
-  // If exists, return with error
-  if (user) {
-    return res.status(400).json({ msg: "Email Already Exists" });
-  }
-
-  // Create a new user, do not save to DB just yet
-  user = new User({ username, email, password });
-
-  //Save user to create unique mongoDB _id
-  await user.save();
-
-  // Create users cart, pass in userID for user Property
-  const cart = new Cart({ user: user._id, items: [] });
-  // Save cart to DB, to create unique mongoDB _id for cart
-  await cart.save();
-
-  //update user with cart ID reference, and save
-  user.cart = cart._id;
-  await user.save();
-
-  res.status(201).json({ userId: user._id, cartId: cart._id });
-});
+// @route: POST /api/user/login
+// @desc:  login user route
+// @access: Public
+router.post("/login", userController.login);
 
 export default router;

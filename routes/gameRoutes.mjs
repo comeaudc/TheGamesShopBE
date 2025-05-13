8ff45cbec22a0caea1799623f79749bfb3cf7971
Dylan Comeau <comeaudc@comcast.net>
2025-05-13 18:00:36 -0400

    Added CRUD routes for game & separated controllers for game

--- routes/gameRoutes.mjs
+++ routes/gameRoutes.mjs
@@ -2,16 +2,31 @@ import express from "express";
 import auth from "../middleware/auth.mjs";
 import adminAuth from "../middleware/adminAuth.mjs";
 import Game from "../models/gameSchema.mjs";
+import gameController from "../controllers/gameController.mjs";
 
 const router = express.Router();
 
 // @route: POST /api/game
 // @desc: create game route
 // @access: Admin
-router.post("/", auth, adminAuth, async (req, res) => {
-  const newGame = await Game.insertOne(req.body);
+router.post("/", auth, adminAuth, gameController.createGame);
 
-  res.json(newGame);
-});
+// @route: GET /api/game
+// @desc: Get All Games
+// @access: Public
+router.get("/", gameController.readGames);
+
+// @route: PUT /api/game/:id
+// @desc: Update Game Information
+// @access: Admin
+
+// @route: Delete /api/game/:id
+// @desc: Delete Game
+// @access: Admin
+
+router
+  .route("/:id", auth, adminAuth)
+  .put(gameController.updatedGame)
+  .delete(gameController.deleteGame);
 
 export default router;
